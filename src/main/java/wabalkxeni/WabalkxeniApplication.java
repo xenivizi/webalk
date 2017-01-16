267c7945e85e68aea1110c049d6a148ce4ec3a0f
xenivizi <xenia.vizi.404@gmail.com>
2017-01-16 20:50:37 +0100

    create

---
+++ src/main/java/wabalkxeni/WabalkxeniApplication.java
@@ -0,0 +1,12 @@
+package wabalkxeni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WabalkxeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WabalkxeniApplication.class, args);
+	}
+}
